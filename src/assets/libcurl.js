612c0186e3b7e026503e6f6ed532c6c26ee71bcb
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-19 17:11:00 -0500

    Refactor load_wasm function to simplify logic
    Removed unnecessary checks for single file mode in load_wasm function.

--- src/assets/libcurl.js
+++ src/assets/libcurl.js
@@ -1455,14 +1455,7 @@ function abort_callback(reason) {
 
 function load_wasm(url) {
   if (wasm_ready) return;
-
-  //skip this if we are running in single file mode
-  if (!wasmBinaryFile || !isDataURI(wasmBinaryFile)) {
-    wasmBinaryFile = url;
-    createWasm();
-    run();  
-  }
-
+  
   return new Promise((resolve, reject) => {
     if (wasm_ready) return resolve();
     api.events.addEventListener("libcurl_load", () => {
